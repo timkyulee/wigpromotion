f5feb027033654c5d6c064a9125f0b6b7733e813
timlee789 <systeco@hotmail.com>
2021-09-01 15:08:48 -0400

    Second version

---
+++ components/CaroucelData.js
@@ -0,0 +1,21 @@
+
+import City from "../Images/city.jpg";
+import Salvador from "../Images/salvador.jpg";
+import Ubc from "../Images/ubc.jpg";
+
+export const images = [
+  { 
+    title: "San Diego", 
+    subtitle: "This is San Diego", 
+    img: "http://destinywig.com/wp-content/uploads/2021/08/weekly1.jpg" },
+  {
+    title: "Salvador, Brazil",
+    subtitle: "The Best City in the World",
+    img:"http://destinywig.com/wp-content/uploads/2021/08/weekly2.jpg",
+  },
+  {
+    title: "UBC (Vancouver)",
+    subtitle: "The University of British Columbia",
+    img: "http://destinywig.com/wp-content/uploads/2021/08/weekly1.jpg",
+  },
+];
