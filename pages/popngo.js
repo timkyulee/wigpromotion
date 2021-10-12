571bc8a45a7f6de8149819ef94c3af26e36b6499
timlee789 <systeco@hotmail.com>
2021-10-12 16:29:20 -0400

    1012-6

--- pages/popngo.js
+++ pages/popngo.js
@@ -1,11 +1,17 @@
 import React from 'react'
-
+import Clacewig from '../components2/Cpopngo';
+import Navbar2 from '../components/Navbar2';
+import styles from '../styles/Home.module.css';
 function popngo() {
     return (
-        <div>
-            
+        <div className={styles.container}>
+             <main className={styles.main}>
+            <Navbar2/>
+            <Clacewig /> 
+            </main>
         </div>
     )
 }
+
 
 export default popngo
--- pages/wig.js
+++ pages/wig.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Clacewig from '../components2/Cwig';
+import Navbar2 from '../components/Navbar2';
+import styles from '../styles/Home.module.css';
 function wig() {
     return (
-        <div>
-            
+        <div className={styles.container}>
+             <main className={styles.main}>
+            <Navbar2/>
+            <Clacewig /> 
+            </main>
         </div>
     )
 }
