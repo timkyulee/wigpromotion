21ac47241fb84634fc4074490c52c8ab4a462dbe
timlee789 <systeco@hotmail.com>
2021-10-13 11:33:05 -0400

    1013-4

--- pages/_app.js
+++ pages/_app.js
@@ -6,18 +6,6 @@ import { Fragment } from 'react';
 function MyApp({ Component, pageProps }) {
   
   return <Fragment>
-    
-    <Head>
-       
-         {/* <!-- open graph description --> */}
-        <meta name="og:title" content="Destiny Wig Promotion"></meta>
-        <meta property="og:description" content="Destiny Wig Promotion 2021 Fall"/>
-        <meta property="og:image" content="https://sopranohair.com/wig/metaimga.jpg"></meta>
-        <meta property="og:url" content="https://destinywig.info"></meta>
-        <meta property="og:image:width" content="300" />
-        <meta property="og:image:height" content="300" />
-        {/* <!-- open graph END --> */}
-      </Head>
   <Component {...pageProps} />
   
   </Fragment>
--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,64 @@ import Head from 'next/dist/shared/lib/head';
 import styles from '../styles/Home.module.css';
 import Navbar from '../components/Navbar2';
 import GalleryAll from '../components/GalleryAll';
-
+import { NextSeo} from 'next-seo';
 
 export default function Home() {
   
  
   return (
     <div >
-       <Head>
-        <title>DestinyWig Promotion</title>
-         {/* <!-- open graph description --> */}
-        <meta name="og:title" content="Destiny Wig Promotion"></meta>
+       <NextSeo
+      title="Destiny Wig Promotion"
+      description="Destiny Wig Promotion 2021 Fall."
+      canonical="https://www.canonical.ie/"
+      openGraph={{
+        url: 'https://destinywig.info',
+        title: 'Destiny Wig Promotion',
+        description: 'Destiny Wig Promotion 2021 Fall.',
+        images: [
+          {
+            url: 'https://sopranohair.com/wig/1sm.jpg',
+            width: 250,
+            height: 333,
+            alt: 'Og Image Alt',
+            type: 'image/jpeg',
+          },
+          {
+            url: 'https://sopranohair.com/wig/1sm.jpg',
+            width: 250,
+            height: 333,
+            alt: 'Og Image Alt Second',
+            type: 'image/jpeg',
+          },
+          { url: 'https://sopranohair.com/wig/metaimga.jpg' },
+          { url: 'https://sopranohair.com/wig/metaimga.jpg' },
+        ],
+        site_name: 'DestinyWig.info',
+      }}
+      twitter={{
+        handle: '@handle',
+        site: '@site',
+        cardType: 'summary_large_image',
+      }}
+    />
+       {/* <Head>
+        
+        <meta property="og:title" content="Destiny Wig Promotion"></meta>
         <meta property="og:description" content="Destiny Wig Promotion 2021 Fall"/>
         <meta property="og:image" content="https://sopranohair.com/wig/metaimga.jpg"></meta>
         <meta property="og:url" content="https://destinywig.info"></meta>
         <meta property="og:image:width" content="300" />
         <meta property="og:image:height" content="300" />
-        {/* <!-- open graph END --> */}
-      </Head>
-      
-      <main className={styles.main}>
-     
-        <Navbar />
        
+      </Head> */}
+      <div className={styles.container}>
+      <main className={styles.main}>
+        <Navbar />
         <GalleryAll />
        
       </main>
- 
+      </div>
     </div>
   )
 }
